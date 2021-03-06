37173764129e310575f11e8147e350d219c680a3
eden takele <se.eden.takele@gmail.com>
2021-03-06 15:29:05 +0300

    new repository created and file copied to submit(Eden)

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+var db;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // create the database
+    let userDB = indexedDB.open('users', 1);
+    
+    userDB.onsuccess = function() {
+        db = userDB.result;
+       
+    }
+
+    userDB.onupgradeneeded = function(event) {
+        var db = event.target.result;
+
+        db.onerror = function() {
+            console.log('Error loading database.');
+        };
+
+        var usersStore = db.createObjectStore('users', {keyPath: 'id',autoIncrement: true});
+        usersStore.createIndex('userName', 'userName', {unique: false});
+        usersStore.createIndex('password', 'password', {unique: false});
+        usersStore.createIndex('phoneNumber', 'phoneNumber', {unique: false});
+       
+
+    }
+
+
+});
